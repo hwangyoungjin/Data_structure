efc2a70914f923ab4a35aed9421fdc826e2532d1
hwangyoungjin <innovation950302@gmail.com>
2020-04-12 18:54:26 +0900

    stackDynamicArray_CAU modified

--- stack/stackDynamicArray_CAU.c
+++ stack/stackDynamicArray_CAU.c
@@ -24,51 +24,51 @@ void Push(Stack* stack, Element data); // add an element to the top of the stack
 Element Pop(Stack* stack); //delete the top element of the stack
 
 
-void expand(Stack* stack);
-Stack* newStack();
-void initStack(Stack* stack, int ssize);
-void disposeStack(Stack* stack);
-void deleteStack(Stack* stack);
+void Expand(Stack* stack); // stack 확장
+Stack* NewStack(); //
+void InitStack(Stack* stack, int ssize); // 초기화
+void DisposeStack(Stack* stack); 
+void DeleteStack(Stack* stack);
 Data* NewData(int num, const char* name);
-void deleteData(Data* data);
-void viewData(Data* data);
+void DeleteData(Data* data);
+void ViewData(Data* data);
 
 
 
 
 int main(void) {
-	Data* data = 0;
-	Stack* stack = newStack();
+	Data* data = NULL;
+	Stack* stack = NewStack();
 	Push(stack, NewData(3, "Rosa"));
-	Push(stack, NewData(5, "John"));
-	Push(stack, NewData(1, "Kim"));
-	Push(stack, NewData(4, "Hwang"));
+	//Push(stack, NewData(5, "John"));
+	//Push(stack, NewData(1, "Kim"));
+	//Push(stack, NewData(4, "Hwang"));
 
 	printf("%-5s %-10s\n", "num", "name");
 	while (!IsEmpty(stack)) { // 스택이 비어있지 않다면 반복
 		data = (Data*)Pop(stack);
-		viewData(data);
-		deleteData(data); //Delete data
+		ViewData(data);
+		DeleteData(data); //Delete data
 	}
-	deleteStack(stack); //Delete stack
+	DeleteStack(stack); //Delete stack
 
 	return 0;
 }
-Stack* newStack() {
+Stack* NewStack() {
 	Stack* stack = (Stack*)malloc(sizeof(Stack));
-	initStack(stack, 1);
+	InitStack(stack, 1);
 	return stack;
 }
-void initStack(Stack* stack, int ssize) {
+void InitStack(Stack* stack, int ssize) {
 	stack->buf = (Element*)malloc(sizeof(Element)* ssize);
 	stack->ssize = ssize;
 	stack->top = -1;
 }
-void disposeStack(Stack* stack) {
+void DisposeStack(Stack* stack) {
 	free(stack->buf);
 }
-void deleteStack(Stack*stack) {
-	disposeStack(stack);
+void DeleteStack(Stack*stack) {
+	DisposeStack(stack);
 	free(stack);
 
 }
@@ -81,7 +81,7 @@ int IsFull(Stack *stack) {
 }
 void Push(Stack *stack, Element data) {
 	if (IsFull(stack)) {
-		expand(stack);
+		Expand(stack);
 	}
 	stack->top++;
 	stack->buf[stack->top] = data;
@@ -96,7 +96,7 @@ Element Pop(Stack *stack) {
 	stack->top--;
 	return re;
 }
-void expand(Stack* stack) {
+void Expand(Stack* stack) {
 	stack->ssize * 2;
 	stack->buf = (Element*)realloc(stack->buf, sizeof(Element) * stack->ssize);
 }
@@ -111,10 +111,10 @@ Data* NewData(int num, const char* name) { //???????????????????????????????????
 }
 //////////////////////////////////////////////////////////////
 
-void deleteData(Data* data) {
+void DeleteData(Data* data) {
 	free(data->name);
 	free(data);
 }
-void viewData(Data* data) {
+void ViewData(Data* data) {
 	printf("%-5d %-10s\n", data->num, data->name);
 }
