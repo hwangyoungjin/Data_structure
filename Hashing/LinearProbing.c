299b8003517bdb119b2fb06ca667342b8cad4150
hwangyoungjin <innovation950302@gmail.com>
2020-06-28 00:14:10 +0900

    LinearProbing modified

--- Hashing/LinearProbing.c
+++ Hashing/LinearProbing.c
@@ -18,13 +18,26 @@ void init(element hashTable[]) {
 }
 
 /*문자로된 key를 숫자로 변환 = > 호너의 방법 이용*/
+//int transform(char* key) {
+//	int num = 0;
+//	while (*key) { //key가 null이 아닐때까지
+//		num = 31 * num + *key++;
+//	}
+//	return num;
+//}
+
+//일반 적인 ascii 값 더하기
 int transform(char* key) {
-	int num = 0;
-	while (*key) { //key가 null이 아닐때까지
-		num = 31 * num + *key++;
+	int num=0;
+	printf("\n%s의 ascii :", key);
+	while (*key) {
+		num += *key++;
 	}
+	printf(" %d\n", num);
 	return num;
 }
+
+
 
 /*제산 함수를 사용한 해쉬 함수*/
 int hash_function(char* key) {
@@ -32,21 +45,22 @@ int hash_function(char* key) {
 	return transform(key) % TABLE_SIZE;
 }
 
-/*linear probing 이용 하여 bucket 비어있는지 check*/
+/*linear probing : bucket 비어있는지 check*/
 void hash_lp_add(element item, element hashTable[]) {
 	int i, hashValue;
-	hashValue = i = hash_function(item.key); //hash address
-	while (hashTable[i].key[0] != NULL) { // Key가 null이면 stop
+	hashValue = i = hash_function(item.key); // item key에 대응하는 hash address
+	while (hashTable[i].key[0] != NULL) { // 첫번째문자 Key[0]이 null이면 아무것도 없기 때문에 stop
 		if (!strcmp(item.key, hashTable[i].key)) { // 두 key값이 동일하면
 			printf("\n탐색키가 중복 되었습니다.");
 			exit(1);
 		}
-		i = (i + 1) % TABLE_SIZE;
-		if (i == hashValue) {
+		i = (i + 1) % TABLE_SIZE; // hash address가 겹치지 않았으므로 다음 버킷에 넣기위해 ++
+		if (i == hashValue) { //증가된 주소가 시작주소로 돌아오면 모든 버킷을 검사해도 남는 공간이 없는것 = 오류상태
 			printf("\nTable FULL!!");
 			exit(1);
 		}
 	}
+	
 	hashTable[i] = item;
 }
 
@@ -54,13 +68,13 @@ void hash_lp_add(element item, element hashTable[]) {
 void hash_lp_search(element item, element hashTable[]) {
 	int i, hashValue;
 	hashValue = i = hash_function(item.key);
-	while (hashTable[i].key[0] != NULL) {
-		if (!strcmp(item.key, hashTable[i].key)) {
-			printf("\n탐색 %s 위치 : %d\n", item.key, i);
+	while (hashTable[i].key[0] != NULL) { //hashTable[i]의 value가 없을때까지 반복 // 여기서 중간에 삭제된공간 있으면 중단되는 단점존재
+		if (!strcmp(item.key, hashTable[i].key)) {  // item의 key값과 hashkey가 동일하면 (0반환)
+			printf("\n탐색 %s 위치 : %d\n", item.key, i);  // 찾은 value 출력하고 return
 			return;
 		}
 		i = (i + 1) % TABLE_SIZE;
-		if (i == hashValue) {
+		if (i == hashValue) { // //증가된 주소가 시작주소로 돌아오면 모든 버킷을 검사해도 남는 공간이 없는것 = 오류상
 			printf("\n찾는 값이 테이블에 없음\n");
 			return;
 		}
@@ -81,17 +95,23 @@ void hash_lp_print(element ht[]) {
 int main() {
 	//포인터 배열
 	char* s[] = { "do","for","if","case","else","return","function" };
+	// do의 ASCII 코드값 (=transform(do))  : 100+111 mod 13 = 3
+	// for의 ASCII 코드값 (=transform(for)) : 102+111+114 mod 13 = 2
+	// ASCII 값으로 나타내면 => s[] = {3,2,12,9,9,9,12} // 9충돌 12충돌
+
 	element e;
+	//모든 단어 add
 	for (int i = 0; i < sizeof(s) / sizeof(s[0]); i++) {
-		strcpy_s(e.key,sizeof(e.key), s[i]);
+		strcpy_s(e.key,sizeof(e.key), s[i]); //s[i]에 들어있는 문자열 복사
 		hash_lp_add(e, hashTable);
 		hash_lp_print(hashTable);
 	}
+
+	//모든 단어 탐색
 	for (int i = 0; i < sizeof(s) / sizeof(s[0]); i++) {
 		strcpy_s(e.key, sizeof(e.key), s[i]);
 		hash_lp_search(e,hashTable);
 	}
-
 
 	return 0;
 }
