32ed4c4ab23e632f0c2478656064ed2620d2639c
hwangyoungjin <innovation950302@gmail.com>
2020-11-24 21:46:52 +0900

    LinkedListStack

---
+++ JAVA/Stack/LinkedListStack.java
@@ -0,0 +1,88 @@
+package report3;
+
+import java.util.Scanner;
+
+class Node{
+	private int data;
+	private Node next;
+	
+	public Node(int data) {
+		this.data = data;
+		this.next = null;
+	}
+	public Node(int data, Node next) {
+		this.data = data;
+		this.next = next;
+	}
+	
+	public int getData() {
+		return this.data;
+	}
+	public Node getNext() {
+		return this.next;
+	}
+	public void setNext(Node next) {
+		this.next = next;
+	}
+}
+class Stack{
+	private Node top;
+	
+	public Stack() {
+		top = null;
+	}
+	
+	public void push(int number) {
+		if(isEmpty()) { // 비어있을 경우
+			top = new Node(number); //노드 생성
+			
+		}
+		else { //하나 이상 있는경우
+			//새 node만들어서 top의 node를 next로 가리키고
+			Node newNode = new Node(number);
+			newNode.setNext(top);
+			//새 node를 다시 top이 가리킨다
+			top = newNode;
+		}
+	}
+	public Node pop() {
+		if(isEmpty()) { //비어있는 경우
+			System.out.println("Error in push-stack is empty");
+			return null;
+		}
+		else { //하나 이상 있는경우
+			Node cur = top;
+			top = top.getNext();
+			return cur;
+		}
+	}
+	
+	public boolean isEmpty() {
+		return top == null;
+	}
+}
+
+public class LinkedListStack{
+	public static void main(String[] args) {
+		Stack stack = new Stack();
+		Scanner scan = new Scanner(System.in);
+		for(int i=0; i<3; i++) {
+			System.out.print("push : " );
+			stack.push(scan.nextInt());
+		}
+		
+		Node current = null;
+		while(true) {
+			System.out.print("pop : ");
+			current = stack.pop(); 
+			if(current != null) {
+				System.out.println(current.getData());
+			}
+			else {
+				break;
+			}
+		}
+		
+		
+	}
+}
